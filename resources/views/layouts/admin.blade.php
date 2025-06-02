7ed31ebf32453b76f87424844174cad7a2cb34fd
Gungcakra <agungcakra888@gmail.com>
2025-06-02 19:10:56 +0800

    update SEO

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,15 +6,15 @@
     <base href="" />
     <title>{{ $title }}</title>
     <meta charset="utf-8" />
-    <meta name="description" content="The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="description" content="Sistem Informasi Kegiatan dan Organisasi Mahasiswa Universitas Cakra. Kelola data organisasi, kegiatan, dan keanggotaan mahasiswa dengan mudah dan efisien." />
+    <meta name="keywords" content="sikom, organisasi mahasiswa, kegiatan mahasiswa, universitas cakra, sistem informasi, manajemen organisasi, event kampus, keanggotaan mahasiswa" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="Metronic - Bootstrap Admin Template, HTML, VueJS, React, Angular. Laravel, Asp.Net Core, Ruby on Rails, Spring Boot, Blazor, Django, Express.js, Node.js, Flask Admin Dashboard Theme & Template" />
-    <meta property="og:url" content="https://keenthemes.com/metronic" />
-    <meta property="og:site_name" content="Keenthemes | Metronic" />
-    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+    <meta property="og:locale" content="id_ID" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="SIKOM - Sistem Informasi Kegiatan dan Organisasi Mahasiswa Universitas Cakra" />
+    <meta property="og:url" content="https://sikom.cakra-portfolio.my.id/login" />
+    <meta property="og:site_name" content="SIKOM STIKOM BALI" />
+    <link rel="canonical" href="https://sikom.cakra-portfolio.my.id/login" />
     <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico')}}" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
